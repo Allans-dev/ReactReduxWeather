5596cfede6763d8ddf9d8a87398038ec78df31f1
Allan.W.C <allan_195@hotmail.com>
2017-07-10 18:29:05 +1000

    Start of location table
    added location component, working on importing to App

---
+++ src/components/location_table.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default class LocationTable extends Component {
+
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Location</th>
+            <th>Forecast</th>
+          </tr>
+        </thead>
+        <tbody>
+          // add in location blocks with Forecast
+        </tbody>
+      </table>
+    );
+  }
+}
