556ab06599633d1e9a21b1080b1301fde812ce29
Allan.W.C <allan_195@hotmail.com>
2017-07-25 15:45:38 +1000

    displays in locationBrief
     - added forecast descriptions and time stamps
     - added global borders to help styling later
     - focus on forecast snapshots instead of averages

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar';
 import LocationTable from './location_table';
-import LocationBrief from '../containers/location_brief';
+
 
 
 export default class App extends Component {
--- src/containers/location_brief.js
+++ src/containers/location_brief.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class LocationBrief extends Component {
+
   renderBrief(locationData) {
     const name = locationData.city.name;
+    const dayOne = locationData.list[4];
+    const dayTwo = locationData.list[12];
+    const dayThree = locationData.list[20];
+    const dayFour = locationData.list[28];
+    const dayFive = locationData.list[36];
+
+    const forecastArr = [dayOne, dayTwo, dayThree, dayFour, dayFive];
+
+
     return (
-      <div key={name}> {name} </div>
+      <div key={name}>
+        <div className="col-md-2 col-sm-2 cityName">
+          {name}
+        </div>
+        {forecastArr.map(function(des){
+          return (
+            <div key={des.dt_txt} className="col-md-2 col-sm-2">
+              {des.dt_txt} <br />
+              {des.weather[0].description}
+            </div>
+          );
+        })}
+      </div>
     );
   }
 
