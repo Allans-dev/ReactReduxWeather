5bfb6bf8000a09d946c23003ec5b72431daed18d
Allan.W.C <allan_195@hotmail.com>
2017-07-29 09:03:46 +1000

    fixing dependencies
     - removed redux-logger
     - installed axios

--- src/store.js
+++ src/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
-import logger from 'redux-logger';
 import promiseMiddleware from 'redux-promise';
 
 
-const middleware = applyMiddleware(promiseMiddleware, thunk, logger);
+const middleware = applyMiddleware(promiseMiddleware, thunk);
 const initialState = {};
 
 export default initialState => (
